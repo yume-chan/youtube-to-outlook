5eddfe4c39479f0b8ba6e0a59026b3a0ff274f4a
Simon Chan <cnsimonchan@live.com>
2019-07-31 13:29:18 +0800

    build(deps): use @yume-chan/async-operation-manager

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 ﻿import { readFileSync, writeFileSync, existsSync } from 'fs';
 
+import { PromiseResolver } from '@yume-chan/async-operation-manager';
+
 import * as Yaml from './yaml';
 import { Google } from "./google";
 import * as MicrosoftGraph from './microsoft';
@@ -53,36 +55,6 @@ async function retry<T>(body: () => Promise<T>, max: number = Infinity): Promise
                 throw e;
             }
         }
-    }
-}
-
-export type PromiseResolverState = 'running' | 'resolved' | 'rejected';
-
-export class PromiseResolver<T>{
-    private _promise: Promise<T>;
-    public get promise(): Promise<T> { return this._promise; }
-
-    private _resolve!: (value?: T | PromiseLike<T>) => void;
-    private _reject!: (reason?: any) => void;
-
-    private _state: PromiseResolverState = 'running';
-    public get state(): PromiseResolverState { return this._state; }
-
-    public constructor() {
-        this._promise = new Promise<T>((resolve, reject) => {
-            this._resolve = resolve;
-            this._reject = reject;
-        });
-    }
-
-    public resolve(value?: T | PromiseLike<T>): void {
-        this._resolve(value);
-        this._state = 'resolved';
-    }
-
-    public reject(reason?: any): void {
-        this._reject(reason);
-        this._state = 'rejected';
     }
 }
 
@@ -454,7 +426,7 @@ export async function getCalendarViewSplit(
     for (const video of videoToUpdate) {
         const channel = config.youtubeChannels.find(x => x.id === video.snippet.channelId);
         if (typeof channel === 'undefined') {
-            console.warn(`unknwon channel ${video.snippet.channelId}`);
+            // console.warn(`unknwon channel ${video.snippet.channelId}`);
             continue;
         }
 
